d1bfcf39eebc50aaa1ee897cf382eb16ae2de795
CangarejoAsul <102051174+CangarejoAsul@users.noreply.github.com>
2024-04-13 12:54:20 +0100

    Update search.py

--- search.py
+++ search.py
@@ -4,32 +4,35 @@ from sqlite3 import connect
 from tkinter import Button, END, Entry, Label, StringVar, Text, Tk
 
 def search():
-  word = findall(r"\w+", wordentry.get().lower())
+  words = findall(r"\w[\w',:.-]*\w|\w", wordentry.get().lower())
   source = findall(r"\w+", sourcelanguageentry.get().lower())
-  targets = findall(r"\w[\w'-]*\w|\w", targetlanguagesentry.get().lower())
-  if len(word) != 1 or len(source) != 1:
+  targets = findall(r"\w+", targetlanguagesentry.get().lower())
+  if len(source) != 1:
     resultstext.delete("1.0", END)
-    resultstext.insert(END, "Please search for exactly one word in exactly one source language.")
+    resultstext.insert(END, "Please choose exactly one source language.")
     return
 
+  arguments = []
   query = ("""
       SELECT DISTINCT
         `sentences-""" + source[0] + """`.id,
         `sentences-""" + source[0] + """`.language,
         `sentences-""" + source[0] + """`.sentence
-      FROM `sentences-""" + source[0] + """`
-      INNER JOIN words
-      ON `sentences-""" + source[0] + """`.id = words.id AND words.word = ?""")
-  list = [word[0]]
+      FROM `sentences-""" + source[0] + """`""")
+  for i, word in enumerate(words):
+    query += ("""
+      INNER JOIN `words-""" + source[0] + """` AS words""" + str(i) + """
+      ON `sentences-""" + source[0] + """`.id = words""" + str(i) + """.id AND words""" + str(i) + """.word = ?""")
+    arguments.append(word)
   for i, target in enumerate(targets):
     query += ("""
-      INNER JOIN links AS links""" + str(i) + """
+      INNER JOIN `links-""" + source[0] + """-""" + target + """` AS links""" + str(i) + """
       ON `sentences-""" + source[0] + """`.id = links""" + str(i) + """.source
       INNER JOIN `sentences-""" + target + """` AS sentences""" + str(i) + """
       ON links""" + str(i) + """.target = sentences""" + str(i) + """.id""")
   query += (""";""")
 
-  cursor.execute(query, tuple(list))
+  cursor.execute(query, tuple(arguments))
   results = cursor.fetchall()
   resultstext.delete("1.0", END)
   if results:
@@ -43,44 +46,46 @@ def close():
   connection.close()
   window.destroy()
 
-if Path("data.db").isfile():
+if Path("data.db").is_file():
   connection = connect("data.db")
   cursor = connection.cursor()
 else:
   connection = connect("data.db")
   cursor = connection.cursor()
 
-  cursor.execute("CREATE TABLE words (id integer NOT NULL, word text NOT NULL);")
-  connection.commit()
-
+  language = {}
   file = open("sentences_detailed.csv", "r", encoding = "utf-8")
   for line in file:
     fields = findall(r"[^\t\n]+", line)
+    language[int(fields[0])] = fields[1]
     try:
       cursor.execute("CREATE TABLE `sentences-" + fields[1] + "` (id integer PRIMARY KEY, language text NOT NULL, sentence text NOT NULL);")
+      cursor.execute("CREATE TABLE `words-" + fields[1] + "` (id integer NOT NULL, word text NOT NULL);")
     except:
       pass
     cursor.execute("INSERT INTO `sentences-" + fields[1] + "` (id, language, sentence) VALUES (?, ?, ?);", (fields[0], fields[1], fields[2]))
-    for word in findall(r"\w[\w'-]*\w|\w", fields[2].lower()):
-      cursor.execute("INSERT INTO words (id, word) VALUES (?, ?);", (fields[0], word))
+    for word in findall(r"\w[\w',:.-]*\w|\w", fields[2].lower()):
+      cursor.execute("INSERT INTO `words-" + fields[1] + "` (id, word) VALUES (?, ?);", (fields[0], word))
   file.close()
-  connection.commit()
-
-  cursor.execute("CREATE TABLE links (source integer NOT NULL, target integer NOT NULL);")
   connection.commit()
 
   file = open("links.csv", "r", encoding = "utf-8")
   for line in file:
     fields = findall(r"[^\t\n]+", line)
-    cursor.execute("INSERT INTO links (source, target) VALUES (?, ?);", (fields[0], fields[1]))
+    try:
+      cursor.execute("CREATE TABLE `links-" + language[int(fields[0])] + "-" + language[int(fields[1])] + "` (source integer NOT NULL, target integer NOT NULL);")
+    except:
+      pass
+    if int(fields[0]) in language and int(fields[1]) in language:
+      cursor.execute("INSERT INTO `links-" + language[int(fields[0])] + "-" + language[int(fields[1])] + "` (source, target) VALUES (?, ?);", (fields[0], fields[1]))
   file.close()
   connection.commit()
 
-  cursor.execute("CREATE TABLE dummy (id integer PRIMARY KEY, language text NOT NULL, sentence text NOT NULL);")
+  cursor.execute("CREATE TABLE version240413 (id integer PRIMARY KEY, language text NOT NULL, sentence text NOT NULL);")
   connection.commit()
 
 try:
-  cursor.execute("SELECT * FROM dummy;")
+  cursor.execute("SELECT * FROM version240413;")
 except:
   print("Please delete data.db.")
   quit()
